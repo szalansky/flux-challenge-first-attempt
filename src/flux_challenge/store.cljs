672c85b4217e3196f0ef9f20ef71e59a89165436
Rafał Adam Szałański <rafal.szalanski@gmail.com>
2015-11-08 11:03:18 +0000

    return null sith lord if master does not have an apprentice

--- src/flux_challenge/store.cljs
+++ src/flux_challenge/store.cljs
@@ -1,7 +1,7 @@
 (ns flux-challenge.store
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [chan <! >!]]))
 
 (def sith-lords (atom []))
 
@@ -15,8 +15,27 @@
           (swap! sith-lords conj (:body response))
           (fetch-until-limit (:url (:apprentice (:body response))) (inc idx) limit)))))
 
+(defn fetch-sith-lord [url]
+  (.log js/console url)
+  (if url
+    (http/get url {:with-credentials? false})
+    (let [null-sith-lord {:body {:null-sith-lord true :homeworld nil :apprentice {:url nil}}}
+          response (chan)]
+      (go []
+          (>! response null-sith-lord))
+      response)))
+
+(defn fetch-sith-lords [url start end]
+  (go-loop [url url
+            start start
+            end end]
+           (when (< start end)
+             (let [response (<! (fetch-sith-lord url))]
+               (swap! sith-lords conj (:body response))
+               (recur (:url (:apprentice (:body response))) (inc start) end)))))
+
 (defn init [url]
   (go []
       (let [response (<! (http/get url {:with-credentials? false}))]
         (swap! sith-lords conj (:body response))
-        (fetch-until-limit (:url (:apprentice (:body response))) 1 8))))
+        (fetch-sith-lords (:url (:apprentice (:body response))) 1 5))))
